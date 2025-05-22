5a942d76cf2cdd93ba1550f90b39abddc1c4c0ee
Manav Shah <shahmanav1308@gmail.com>
2025-05-22 14:31:00 +0530

    Configuring railway

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }))
 
 const conn = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
 
 conn.connect(err => {
